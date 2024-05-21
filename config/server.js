718bc28904376f3bc7b70ccb10abca3516853e4a
Raymond <njagala.raymond@gmail.com>
2024-05-21 08:25:06 +0300

    added security  keys

--- config/server.js
+++ config/server.js
@@ -4,6 +4,11 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  config: {
+    // ... other configurations
+    // Add the app keys here
+    appKeys: ['72edd23107f434afb065043219b7d136eddc33ebe2dbccc9355100bfa9446fa1', '465d9a86f768b11b83b9927e376a12fc8358a5f981b986e7e2a825a9c6adc053'],
+  },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
